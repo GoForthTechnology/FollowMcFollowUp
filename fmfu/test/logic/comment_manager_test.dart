b9ecec2267d5d6008a928e34c338ea79761e225f
Parker Roth <parkeroth@gmail.com>
2022-10-11 19:18:12 +0200

    Initial test coverage for comment manager.

--- fmfu/test/logic/comment_manager_test.dart
+++ fmfu/test/logic/comment_manager_test.dart
@@ -27,4 +27,35 @@ void main() {
     expect(layout.getCommentsForPage(4).length, 1);
     expect(layout.getCommentsForPage(5).length, 0);
   });
+
+  final dummyId = CommentId(
+    boxId: BoxId(
+      followUp: 0,
+      itemId: ItemId(
+        section: 4,
+        subSection: "A",
+      ),
+    ),
+    index: 0,
+  );
+
+  group("CommentRowData", () {
+    group("fromComment", () {
+      test("Splits longs lines", () {
+        var aLongString = List.generate(50, (index) => "asdf").join(" ");
+        expect(CommentRowData.fromComment(FollowUpFormComment(
+          id: dummyId,
+          date: DateTime.now(),
+          problem: aLongString,
+          planOfAction: "a plan",
+        )).length, 2);
+        expect(CommentRowData.fromComment(FollowUpFormComment(
+          id: dummyId,
+          date: DateTime.now(),
+          problem: "a problem",
+          planOfAction: aLongString,
+        )).length, 2);
+      });
+    });
+  });
 }
