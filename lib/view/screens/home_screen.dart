a961ba4502e11b19508cd0eda9877f1fa084f3d8
Parker Roth <parkeroth@gmail.com>
2022-11-25 15:54:40 +0100

    New tiled home screen.

--- lib/view/screens/home_screen.dart
+++ lib/view/screens/home_screen.dart
@@ -5,12 +5,31 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fmfu/routes.gr.dart';
 import 'package:fmfu/utils/screen_widget.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class HomeScreen extends ScreenWidget {
   static const String routeName = "home";
 
   HomeScreen({Key? key}) : super(key: key);
+
+  Widget _tile({
+    required Color color,
+    required String title,
+    required IconData icon,
+    required String text,
+    required Function() onClick,
+  }) {
+    return GestureDetector(onTap: onClick, child: Container(
+      padding: const EdgeInsets.all(8),
+      color: color,
+      child: Column(mainAxisSize: MainAxisSize.min, children: [
+        Text(title, style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+        const Spacer(),
+        Icon(icon, size: 50,),
+        const Spacer(),
+        Text(text),
+      ]),
+    ));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,144 +45,98 @@ class HomeScreen extends ScreenWidget {
           }, icon: const Icon(Icons.logout,), tooltip: "Sign Out",),
         ],
       ),
-      body: Center(child: Padding(padding: const EdgeInsets.all(20), child: Column(
-        children: [
-          const Text("For Students", style: TextStyle(fontStyle: FontStyle.italic)),
-          openChartEditor(context),
-          staticExercises(context),
-          dynamicExercises(context),
-          joinExercise(context),
-          const Divider(),
-          const Text("For Educators", style: TextStyle(fontStyle: FontStyle.italic)),
-          manageProgram(context),
-          const Divider(),
-          const Text("Under Construction", style: TextStyle(fontStyle: FontStyle.italic)),
-          openFollowUpForm(context),
-          // TODO: re-enable once the reporting user is set up
-          //reportIssue(context),
+      body: Center(child: ConstrainedBox(constraints: BoxConstraints.tightFor(width: 400), child: GridView.extent(
+        primary: false,
+        padding: const EdgeInsets.all(16),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        maxCrossAxisExtent: 300.0,
+        children: <Widget>[
+          _tile(
+            color: Colors.lightBlue,
+            icon: Icons.grid_on,
+            title: "Chart Editor",
+            text: "Some text about what assignments are.",
+            onClick: () {
+              AutoRouter.of(context).push(const ChartEditorPageRoute());
+            }
+          ),
+          _tile(
+            color: Colors.lightBlue,
+            icon: Icons.assignment,
+            title: "Assignments",
+            text: "Some text about what assignments are.",
+            onClick: () {
+              // TODO: open assignment screen
+            }
+          ),
+          _tile(
+            color: Colors.lightBlue,
+            icon: Icons.group,
+            title: "Group Exercise",
+            text: "Join a group exercise.",
+            onClick: () {
+              showDialog(context: context, builder: (context)  {
+                var formKey = GlobalKey<FormState>();
+                saveForm() {
+                  if (formKey.currentState!.validate()) {
+                    formKey.currentState!.save();
+                  }
+                }
+                return AlertDialog(
+                  title: const Text("Enter Exercise ID"),
+                  content: Form(
+                    key: formKey,
+                    child: TextFormField(
+                      validator: (value) {
+                        return "Exercise not found";
+                      },
+                      onFieldSubmitted: (value) => saveForm(),
+                      onSaved: (value) {
+                        // TODO: trigger exercise
+                      },
+                    ),
+                  ),
+                  actions: [
+                    TextButton(
+                      onPressed: () => saveForm(),
+                      child: const Text("Submit"),
+                    ),
+                  ],
+                );
+              });
+            },
+          ),
+          _tile(
+            color: Colors.lightBlue,
+            icon: Icons.person,
+            title: "Exercises",
+            text: "Get some extra practice with generic scenarios and specific exercises.",
+            onClick: () {
+              // TODO: open exercise screen
+            },
+          ),
+          _tile(
+            color: Colors.grey[300]!,
+            icon: Icons.engineering,
+            //icon: Icons.edit_calendar,
+            title: "Manage Program",
+            text: "Under construction.",
+            onClick: () {
+              // TODO: open manage program screen
+            },
+          ),
+          _tile(
+            color: Colors.grey[300]!,
+            icon: Icons.engineering,
+            title: "Follow Up Form",
+            text: "Under construction.",
+            onClick: () {
+              AutoRouter.of(context).push(FupFormScreenRoute());
+            },
+          ),
         ],
       ))),
-    );
-  }
-
-  Widget manageProgram(BuildContext context) {
-    return ButtonWidget(
-      title: "Group Exercises",
-      onPressed: () {
-        AutoRouter.of(context).push(GroupExerciseListScreenRoute());
-      },
-      backgroundColor: Colors.pinkAccent,
-    );
-  }
-
-  Widget staticExercises(BuildContext context) {
-    return ButtonWidget(
-      title: "Static Exercises",
-      onPressed: () {
-        AutoRouter.of(context).push(StaticExerciseListScreenRoute());
-      },
-    );
-  }
-
-  Widget dynamicExercises(BuildContext context) {
-    return ButtonWidget(
-      title: "Dynamic Exercises",
-      onPressed: () {
-        AutoRouter.of(context).push(DynamicExerciseListScreenRoute());
-      },
-    );
-  }
-
-  Widget joinExercise(BuildContext context) {
-    return ButtonWidget(
-      title: "Join Group Exercise",
-      onPressed: () {
-        showDialog(context: context, builder: (context)  {
-          var formKey = GlobalKey<FormState>();
-          saveForm() {
-            if (formKey.currentState!.validate()) {
-              formKey.currentState!.save();
-            }
-          }
-          return AlertDialog(
-            title: const Text("Enter Exercise ID"),
-            content: Form(
-              key: formKey,
-              child: TextFormField(
-                validator: (value) {
-                  return "Exercise not found";
-                },
-                onFieldSubmitted: (value) => saveForm(),
-                onSaved: (value) {
-                  // TODO: trigger exercise
-                },
-              ),
-            ),
-            actions: [
-              TextButton(
-                onPressed: () => saveForm(),
-                child: const Text("Submit"),
-              ),
-            ],
-          );
-        });
-      },
-    );
-  }
-
-  Widget openChartEditor(BuildContext context) {
-    return ButtonWidget(
-      title: "Chart Editor",
-      onPressed: () {
-        AutoRouter.of(context).push(const ChartEditorPageRoute());
-      },
-    );
-  }
-
-  Widget openFollowUpForm(BuildContext context) {
-    return ButtonWidget(
-      title: "FupFormScreen",
-      onPressed: () {
-        AutoRouter.of(context).push(FupFormScreenRoute());
-      },
-      backgroundColor: Colors.grey,
-    );
-  }
-
-  Widget reportIssue(BuildContext context) {
-    return ButtonWidget(
-      title: "Report Issue",
-      backgroundColor: Colors.pinkAccent,
-      onPressed: () async {
-        if (!await launchUrl(_newGithubIssueURL)) {
-          throw Exception("Could not launch URL: $_newGithubIssueURL");
-        }
-      },
-    );
-  }
-}
-
-final Uri _newGithubIssueURL = Uri.parse('https://github.com/BloomCycleCare/FollowMcFollowUp/issues/new');
-
-class ButtonWidget extends StatelessWidget {
-  final String title;
-  final Color? backgroundColor;
-  final VoidCallback onPressed;
-
-  const ButtonWidget({super.key, required this.title, required this.onPressed, this.backgroundColor});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(10),
-      child: ElevatedButton(
-        onPressed: onPressed,
-        style: ElevatedButton.styleFrom(
-          backgroundColor: backgroundColor,
-        ),
-        child: Padding(padding: const EdgeInsets.all(10), child: Text(
-          title, style: const TextStyle(fontSize: 18))),
-      ),
     );
   }
 }
