f4d0a3618a33cc8f5fde386fa3ead6a392a0c825
Parker Roth <parkeroth@gmail.com>
2022-11-27 14:05:54 +0100

    Tiled exercise list screen.

--- lib/view/screens/exercise_list_screen.dart
+++ lib/view/screens/exercise_list_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:fmfu/logic/exercises.dart';
@@ -8,12 +10,74 @@ import 'package:provider/provider.dart';
 class ExerciseScreen extends StatelessWidget {
   const ExerciseScreen({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Exercises"),
+      ),
+      body: Consumer<ExerciseListViewModel>(builder: (context, model, child) {
+        model.getExercises(ExerciseType.dynamic);
+        var customExercises = Future.wait([
+          model.getCustomExercises(ExerciseType.dynamic),
+          model.getCustomExercises(ExerciseType.static),
+        ]);
+        return LayoutBuilder(builder: (context , constraints ) => FutureBuilder(
+          future: customExercises,
+          builder: ((context, snapshot) {
+            Widget tile(Exercise e, bool isCustom) {
+              Color color;
+              if (!e.enabled) {
+                color = Colors.grey[300]!;
+              } else if (isCustom) {
+                color = Colors.pinkAccent;
+              } else {
+                color = Colors.lightBlue;
+              }
+              return _tile(
+                color: color,
+                onClick: e.enabled ? () {
+                  AutoRouter.of(context).push(
+                      FollowUpSimulatorPageRoute(exerciseState: e.getState()));
+                } : () {
+                  const snackBar = SnackBar(
+                    content: Text("Coming Soon!"),
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                },
+                icon: e.enabled ? Icons.person : Icons.engineering,
+                title: e.name,
+                text: e.enabled ? "Some description here." : "Under Construction",
+              );
+            }
+            var scenarios = dynamicExerciseList.map((e) => tile(e, false)).toList();
+            var exercises = staticExerciseList.map((e) => tile(e, false)).toList();
+            if (snapshot.hasData) {
+              List<List<Exercise>> customExercises = snapshot.data! as List<List<Exercise>>;
+              scenarios.addAll(customExercises[0].map((e) => tile(e, true)).toList());
+              exercises.addAll(customExercises[1].map((e) => tile(e, true)).toList());
+            }
+            final scrollView = CustomScrollView(
+              slivers: [
+                _makeHeader("Scenarios"),
+                _makeGrid(scenarios, constraints),
+                _makeHeader("Exercises"),
+                _makeGrid(exercises, constraints),
+              ],
+            );
+            return Center(child: scrollView);
+          }),
+        ));
+      }),
+    );
+  }
+
   Widget _tile({
     required Color color,
     required String title,
     required IconData icon,
     required String text,
-    required Function() onClick,
+    required Function()? onClick,
   }) {
     return GestureDetector(onTap: onClick, child: Container(
       padding: const EdgeInsets.all(8),
@@ -28,64 +92,62 @@ class ExerciseScreen extends StatelessWidget {
     ));
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Exercises"),
+  SliverPersistentHeader _makeHeader(String headerText) {
+    return SliverPersistentHeader(
+      delegate: _SliverAppBarDelegate(
+        minHeight: 60.0,
+        maxHeight: 60.0,
+        child: Center(child: Text(headerText, style: const TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ))),
       ),
-      body: Consumer<ExerciseListViewModel>(builder: (context, model, child) {
-        model.getExercises(ExerciseType.dynamic);
-        var customExercises = Future.wait([
-          model.getCustomExercises(ExerciseType.dynamic),
-          model.getCustomExercises(ExerciseType.static),
-        ]);
-        return FutureBuilder(
-          future: customExercises,
-          builder: ((context, snapshot) {
-            Widget tile(Exercise e, bool isCustom) {
-              return _tile(
-                color: isCustom ? Colors.pinkAccent : Colors.lightBlue,
-                onClick: () {
-                  AutoRouter.of(context).push(
-                      FollowUpSimulatorPageRoute(exerciseState: e.getState()));
-                },
-                icon: Icons.person,
-                title: e.name,
-                text: "",
-              );
-            }
-            var scenarios = dynamicExerciseList.map((e) => tile(e, false)).toList();
-            var exercises = staticExerciseList.map((e) => tile(e, false)).toList();
-            if (snapshot.hasData) {
-              List<List<Exercise>> customExercises = snapshot.data! as List<List<Exercise>>;
-              scenarios.addAll(customExercises[0].map((e) => tile(e, true)).toList());
-              exercises.addAll(customExercises[1].map((e) => tile(e, true)).toList());
-            }
-            return Center(child: ConstrainedBox(constraints: const BoxConstraints.tightFor(width: 400), child: Column(children: [
-              const Text("Scenarios", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),),
-              Expanded(child: GridView.extent(
-                primary: false,
-                padding: const EdgeInsets.all(16),
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-                maxCrossAxisExtent: 300.0,
-                children: scenarios,
-              )),
-              const Text("Exercises", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),),
-              Expanded(child: GridView.extent(
-                primary: false,
-                padding: const EdgeInsets.all(16),
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-                maxCrossAxisExtent: 300.0,
-                children: exercises,
-              )),
-            ])));
-          }),
-        );
-      }),
     );
   }
 
+  Widget _makeGrid(List<Widget> children, BoxConstraints constraints) {
+    const gridWidth = 400;
+    double paddingWidth = max((constraints.maxWidth - gridWidth) / 2, 0);
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(
+        vertical: 15,
+        horizontal: max(paddingWidth, 15),
+      ),
+      sliver: SliverGrid.count(
+        crossAxisCount: 2,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        children: children,
+      ),
+    );
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate({
+    required this.minHeight,
+    required this.maxHeight,
+    required this.child,
+  });
+  final double minHeight;
+  final double maxHeight;
+  final Widget child;
+  @override
+  double get minExtent => minHeight;
+  @override
+  double get maxExtent => max(maxHeight, minHeight);
+  @override
+  Widget build(
+      BuildContext context,
+      double shrinkOffset,
+      bool overlapsContent)
+  {
+    return SizedBox.expand(child: child);
+  }
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return maxHeight != oldDelegate.maxHeight ||
+        minHeight != oldDelegate.minHeight ||
+        child != oldDelegate.child;
+  }
 }
